56173704690032aa5308289ebcaa703fca7875c4
Ozan Oezyurt <ozan.oezyurt@icloud.com>
2023-05-10 10:46:10 +0200

    ternary

--- script_01.js
+++ script_01.js
@@ -40,13 +40,20 @@ console.log("------------------");
 // if(true)
 // if(false)
 // if(isJohnOlder)
-if(ageJohn > ageMark)
-{
-    console.log("John ist älter.");
-} 
-else 
-{
-    console.log("John ist jünger.");
-}
+// if(ageJohn > ageMark)
+// {
+//     console.log("John ist älter.");
+// } 
+// else 
+// {
+//     console.log("John ist jünger.");
+// }
+
+/************  Ternäre (ternary) Schreibweise ************/
+console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+/*********** ELSE - IF ************/
+
+
 
 
