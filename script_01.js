8c1469d8ac8c7f00c322d35517753d7f175d02f3
Ozan Oezyurt <ozan.oezyurt@icloud.com>
2023-05-10 12:14:16 +0200

    Fallunterscheidung / SWITCH|CASE 2

--- script_01.js
+++ script_01.js
@@ -3,22 +3,22 @@
 /*Entscheidungsstrukturen | control flow */
 
 // Deklaration + Assignment
-const ageJohn = 25;
-const ageMark = 30;
+// const ageJohn = 30;
+// const ageMark = 35;
 
 // Deklaration
-let isJohnOlder, isJohnEqual;
+// let isJohnOlder, isJohnEqual;
 
 // Test | Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
+// isJohnOlder = (ageJohn > ageMark);
+// isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
 
 /************ IF  ************/
 // TINA --> There is no alternative!
@@ -55,19 +55,72 @@ console.log("------------------");
 /************ IF - ELSE IF  ************/
 // mit alternativen Fällen (älter, gleich alt, jünger)
 
-if(isJohnOlder) //1.Test
-{
-    console.log("John ist älter.");
-} 
-else if(isJohnEqual)// 1. ...n Alternative
-{
-    console.log("John ist gleichalt.");
-} 
+// if(isJohnOlder) //1.Test
+// {
+//     console.log("John ist älter.");
+// } 
+// else if(isJohnEqual)// 1. ...n Alternative
+// {
+//     console.log("John ist gleichalt.");
+// } 
 
-else // wenn alle vorherigen Tests fehlschlagen
+// else // wenn alle vorherigen Tests fehlschlagen
+// {
+//     console.log("John ist jünger.");
+// }
+
+
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+const firstName = "Jane";
+let job; 
+
+// job = "driver";      // .. fährt TAXI! / UBER
+job = "diver";       // .. taucht im Rhein! 
+// job = "researcher"   // .. untersucht den Dschungel! 
+// job = "gettinglost"  //  .. lernt Tarzan kennen!
+// job = "pilot";      // .. macht etwas anderes! --> default
+job = "teacher";    // .. unterrichtet!
+// job = "instructor"; // .. unterrichtet!
+
+
+
+switch (job)
 {
-    console.log("John ist jünger.");
+
+    case "driver": // ( == )
+        console.log(firstName + " fährt TAXI!")
+        break;
+        
+    case "diver": // 
+        console.log(firstName + " tauch im Rhein!")
+        break;   
+        
+    case "researcher": // 
+        console.log(firstName + " untersucht den Dschungel")
+        break; 
+
+    case "teacher": //  ( == )
+    case "instructor": // ( == )    
+        console.log(firstName + " unterrichtet")
+        break;  
+    
+    case "pilot": // 
+        console.log(firstName + " macht etwas anderes!")
+        break;
+    
+    case "gettinglost": // 
+        console.log(firstName + " lernt Tarzan kennen!")
+        break;  
+        
+      default:
+        console.log(firstName + " macht was anderes!")
+        break;
 }
+/********  Fallunterscheidung / SWITCH|CASE 2 *******/
+
+
 
 
 
